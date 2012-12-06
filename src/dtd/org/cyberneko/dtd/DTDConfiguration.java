38a8a1129875f004293ba317c486f4aa33f28b8d
Andy Clark <andclark@linkedin.com>
2012-12-06 13:08:50 -0800

    Change license from Apache "style" to Apache 2.0.

--- src/dtd/org/cyberneko/dtd/DTDConfiguration.java
+++ src/dtd/org/cyberneko/dtd/DTDConfiguration.java
@@ -1,7 +1,7 @@
 /*
  * (C) Copyright 2002-2003, Andy Clark.  All rights reserved.
  *
- * This file is distributed under an Apache style license. Please
+ * This file is distributed under an Apache license. Please
  * refer to the LICENSE file for specific details.
  */
 
